3eb50b16a2059048bfd77b98c0c363c48c4b7bdb
Tucker <tuckerk23@gmail.com>
2017-12-20 21:09:10 -0700

    logic

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+// var terms = "&"
+// Built by LucyBot. www.lucybot.com
+
+
+var search = "q=" + $("#search").val();
+var resultNumber = "&fl=" + $("#resultNumber").val();
+var beginDate = "&begin_date=" + $("#beginDate").val();
+var endDate = "&end_date=" + $("#endDate").val();
+var url =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + search + resultNumber + "api-key=c35b668c9c3b49628890ac0162dee3a0";
+$.ajax({
+  url: url,
+  method: "GET"
+})
+  .done(function(result) {
+    for (let i = 0; i < result.response.docs.length; i++) {
+      const element = result.response.docs[i];
+
+      console.log(result.response.docs[i].headline.main);
+      console.log(result.response.docs[i].snippet);
+      console.log(result.response.docs[i].web_url);
+    }
+  })
+  .fail(function(err) {
+    throw err;
+  });
